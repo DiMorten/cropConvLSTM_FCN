648e897e5e276c75c107f665b683651b50a8e744
Jorg <cmjorgeandres@gmail.com>
2021-05-13 21:17:49 -0300

    Paso 4 ok. convlstm with input label ok
    Before data augmentation

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ import glob
 import numpy as np
 from generator import DataGenerator
 from timedistributed_generator import TimeDistributedDataGenerator
-from models import cnn, Monitor, f1_mean, UUnetConvLSTM, BUnet4ConvLSTM, UUnet4ConvLSTM, UUnet3DConvLSTM, simplefcn_t, simplefcn, cnn_t
+from models import cnn, Monitor, f1_mean, UUnetConvLSTM, BUnet4ConvLSTM, UUnet4ConvLSTM, UUnet3DConvLSTM, simplefcn_t, simplefcn, cnn_t, cnn_rnn_t
 from deeplab_versions import DeepLabVersions, DeepLabConvLSTM
 from keras.utils import plot_model
 from keras.optimizers import SGD, Adadelta, Adagrad, Adam
@@ -456,6 +456,8 @@ if __name__ == '__main__':
                 model = simplefcn(img_shape=dim, class_n=params.classes) 
             elif params.model == 'cnn_t':
                 model = cnn_t(img_shape=dim, nb_classes=params.classes)  
+            elif params.model == 'cnn_rnn_t':
+                model = cnn_rnn_t(img_shape=dim, nb_classes=params.classes)  
 
                 
             print(model.summary())
